ee8465649c6d1eb3e1f99167a2227e362d00a35c
Joshua <joshjoshuap1@gmail.com>
2022-11-16 16:05:18 +0800

    string methods

--- String.php
+++ String.php
@@ -5,9 +5,20 @@ $name = "Joshua";
 echo strlen($name); // 6
 echo strtoupper($name); // JOSHUA
 echo strtolower($name); // joshua
+echo substr($name, 1, 3); // osh
+echo strpos($name, 'J'); // 0
+echo trim('Hello     World'); // Hello World
+echo str_replace('Joshua', 'Jose', $name); // Jose
+echo is_string($name); // 1
+
+// Compress - Uncompressed
+$sampleText = 'Hello There';
+$compressedText = gzcompress($sampleText); // Compress random
+echo $compressedText;
+echo gzuncompress($compressedText); // Hello There
 
 // Template Literals
 $firstname = 'Joshua';
 $lastname = 'P';
 $fullname = "$firstname $lastname";
-echo $fullname;
+echo $fullname; // Joshua P
