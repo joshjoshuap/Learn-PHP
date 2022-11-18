8c6787a395013f4a1730950059a849b47ddd031a
Joshua <joshjoshuap1@gmail.com>
2022-11-18 16:32:55 +0800

    superglobal server

---
+++ Superglobal.php
@@ -0,0 +1,11 @@
+<ul style="list-style: none;">
+    <li>Host: <?php echo $_SERVER['HTTP_HOST'] ?></li>
+    <li>Document Root: <?php echo $_SERVER['DOCUMENT_ROOT'] ?></li>
+    <li>Server Port: <?php echo $_SERVER['SERVER_NAME'] ?></li>
+    <li>Server Port: <?php echo $_SERVER['SERVER_PORT'] ?></li>
+    <li>Current File Dir: <?php echo $_SERVER['PHP_SELF'] ?></li>
+    <li>Request URI: <?php echo $_SERVER['REQUEST_URI'] ?></li>
+    <li>Server Software: <?php echo $_SERVER['SERVER_SOFTWARE'] ?></li>
+    <li>Remote Address: <?php echo $_SERVER['REMOTE_ADDR'] ?></li>
+    <li>Remote Port: <?php echo $_SERVER['REMOTE_PORT'] ?></li>
+</ul>
