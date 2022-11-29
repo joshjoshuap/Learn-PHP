fd483c220bab640e12f69380beb61be834f79ae2
Joshua <joshjoshuap1@gmail.com>
2022-11-29 17:31:20 +0800

    routes, routing

--- laravel-basics/routes/web.php
+++ laravel-basics/routes/web.php
@@ -13,6 +13,50 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// // Supported Routes
+// Route::get();
+// Route::post();
+// Route::put();
+// Route::patch();
+// Route::delete();
+
+// Route::match(['get', 'post'], '/', function () {
+//     return 'Get and Post';
+// });
+
+// Route::any('/test', function () {
+//     return view('test');
+// });
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return view('test'); // returning view
+});
+
+Route::get('/get-data', function () {
+    return response('Test Data', 200)
+        ->header('Content-type', 'text/plain');
+}); // response text with data
+
+Route::get('/get-json', function () {
+    return response()->json(['name' => 'Joshua', 'age' => 22]);
+}); //returning json with data
+
+Route::get('/user/{id}/{name}', function ($id, $name) {
+    return response($id . ' ' . $name, 200);
+}); // getting url paramaters
+
+Route::get('/downloading-file', function () {
+    $path = public_path() . '/sample-download.txt';
+    $name = 'sample-download.txt';
+    $headers = array(
+        'Content-type : appplication/text-plain',
+    );
+    return response()->download($path, $name, $headers);
+}); // response and automatically download
+
+
+// Route::redirect('/test', '/'); // this will redirect /test to homepage, note declare after the other route
