0ba4212c36eb4738e0e8f627f40b4ba9b43df451
Joshua <joshjoshuap1@gmail.com>
2022-11-30 15:19:29 +0800

    add tailwind css

--- laravel-basics/resources/views/user.blade.php
+++ laravel-basics/resources/views/user.blade.php
@@ -6,13 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>User </title>
-
+    @vite('resources/css/app.css')
 </head>
 
 <body>
 
-
-    <h1>Hello {{$name}}</h1>
+    <h1 class="text-3xl font-bold underline">Hello {{$name}}</h1>
     <h3>Age: {{$age}}</h3>
     <h3>Address: {{$address}}</h3>
 </body>
